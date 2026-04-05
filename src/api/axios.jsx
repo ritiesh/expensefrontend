d7b24b00fd572c7c9a2f7dc3d56a79bf1f88f708
Ritiesh <ritiesh.it20@bitsathy.ac.in>
2026-04-05 12:51:25 +0530

    changed port backend

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://expensetracker-backend-i6dc.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
